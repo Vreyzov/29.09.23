f659f27b6e033c804db48e85cf185ada7e776513
Vreyzov <115491934+Vreyzov@users.noreply.github.com>
2023-09-28 20:04:48 +0300

    Add files via upload

---
+++ №6.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите отметку");
+double x = Convert.ToDouble(Console.ReadLine());
+switch (x)
+{
+
+    
+    case 1:
+        Console.WriteLine("плохо");
+        break;
+    case 2:
+        Console.WriteLine("неудовлетворительно");
+        break;
+    case 3:
+        Console.WriteLine("удовлетворительно");
+        break;
+    case 4:
+        Console.WriteLine("хорошо");
+        break;
+    case 5:
+        Console.WriteLine("отлично");
+        break;
+   
+    default:
+        Console.WriteLine("Введите отметку от 1 до 5");
+        break;
+
+}
